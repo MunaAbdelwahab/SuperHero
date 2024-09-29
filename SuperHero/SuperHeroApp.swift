47707cea84137d430bf8808e6585328a5c4a1a12
Muna Abdelwahab <69257240+MunaAbdelwahab@users.noreply.github.com>
2024-09-29 17:11:15 +0300

    Initial Commit

---
+++ SuperHero/SuperHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroApp.swift
+//  SuperHero
+//
+//  Created by MSOL on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
